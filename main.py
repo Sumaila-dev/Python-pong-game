1016dbbc1ec3c6214156eb825a81304909309b69
Sumaila-dev <82516089+Sumaila-dev@users.noreply.github.com>
2023-07-11 20:38:49 +0000

    moving the ball

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from turtle import Screen,Turtle
 from paddle import Paddle
 from ball import Ball
+import time
+
 
 screen = Screen()
 screen.bgcolor("black")
@@ -21,7 +23,8 @@ screen.onkey(l_paddle.go_down(), "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-
+    ball.move()
 
 screen.exitonclick()
